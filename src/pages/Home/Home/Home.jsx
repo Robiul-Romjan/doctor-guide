339302aa2ee5eabac9ade48e1f3e160a1bfb712f
robiul-romjan <robiulromjan7@gmail.com>
2023-06-21 01:01:09 +0600

    medical care section complite

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,18 @@
 import Banner from "../Banner/Banner";
+import MedicalCare from "../MedicalCare/MedicalCare";
 import Schedule from "../Schedule/Schedule";
 import Solution from "../Solution/Solution";
 
+
+
 const Home = () => {
+  
     return (
         <div className="relative mt-[63px] my-container">
             <Banner />
             <Solution />
             <Schedule />
+            <MedicalCare />
         </div>
     );
 };
---
+++ src/pages/Home/MedicalCare/MedicalCare.jsx
@@ -0,0 +1,41 @@
+import img from '../../../assets/images/image.png'
+import img1 from '../../../assets/images/image-1.png'
+import img2 from '../../../assets/images/image-2.png'
+import img3 from '../../../assets/images/image-3.png'
+import img4 from '../../../assets/images/image-4.png'
+import img5 from '../../../assets/images/image-5.png'
+import img6 from '../../../assets/images/image-6.png'
+import img7 from '../../../assets/images/image-4.png'
+
+const MedicalCare = () => {
+
+    const data = [
+        {name: "Orthopedic Clinic", image: img },
+        {name: "Cardiac Surgery", image: img2 },
+        {name: "Cancer Screening", image: img3 },
+        {name: "Neurology", image: img4 },
+        {name: "Pediatric & Adult Audiology", image: img5 },
+        {name: "Neurology", image: img6 },
+        {name: "Hearing Tests & Aids", image: img1 },
+        {name: "Orthopedic Clinic", image: img7 },
+    ];
+   
+    return (
+        <div className="my-container mt-[107px]">
+            <div className="text-center">
+                <h2 className="text-[#273240] text-[40px] font-bold">Our Medical Care</h2>
+                <p className="text-sm text-[#273240] font-medium">Services We Provide</p>
+                <div className='grid md:grid-cols-3 lg:grid-cols-4 gap-[30px] mt-[70px]'>
+                    {
+                        data.map((service, i) => <div className='text-center text-white' key={i}>
+                            <img className='h-[227px]' src={service.image} alt="" />
+                            <p className='bg-[#6E27E0] py-[12px] text-[20px] font-medium'>{service.name}</p>
+                        </div>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MedicalCare;
